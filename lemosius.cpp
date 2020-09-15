b99c0ef8a7dca6dc5e80c87ddd12f5aa86c7599f
rccuam <racc@correo.azc.uam.mx>
2020-09-15 11:17:11 -0500

    No más newline significativo, cambios en try-catch de main
    Salvo comentarios, el salto de línea no debe ser el fin de una sentencia: o se van a usar puntos y comas para ahorrarnos eso o no se van a usar (sí se van a usar)
    
    try con mejor estilo en main

--- lemosius.cpp
+++ lemosius.cpp
@@ -4,29 +4,26 @@
 #include"error_handling.h"
 
 
-int main(int argc, char *argv[]) {
-	std::string text;
-	try {
-		text = read_file( argv[1] );
-		const char* ini = text.c_str();
-		try {
-			std::vector<token_anotada> preludio = prelude_lexer( ini );		
-			for(auto& token : preludio) {
-				std::cout << token.type << "\n";
-				std::cout << token.original << "\n";
-			}
-			auto tokens = preludio.begin();
-			header(tokens);
-		}
-		catch(const std::pair<token_anotada, const char*>& e) {
-			error_report(std::cout, text.c_str(), text.c_str() + text.length(), e);
-		}
-	}
-	catch(const std::ifstream::failure& e) {
-		std::cout << "Error al leer el archivo: " << e.what() << "\n";
-	}
-	catch(const std::filesystem::filesystem_error& e) {
-		std::cout << "Error al abrir el archivo " << e.path1() << ": " << e.what() << "\n";
-	}
+int main(int argc, char *argv[])
+try {
+	std::string text = read_file( argv[1] );
+   try {
+      const char* ini = text.c_str();
+      std::vector<token_anotada> preludio = prelude_lexer( ini );
+      for(auto& token : preludio) {
+         std::cout << token.type << "\n";
+         std::cout << token.original << "\n";
+      }
+      auto tokens = preludio.begin();
+      header(tokens);
+   }
+   catch(const std::pair<token_anotada, const char*>& e) {
+      error_report(std::cout, text.c_str(), text.c_str() + text.length(), e);
+   }
 }
-	
+catch(const std::ifstream::failure& e) {
+   std::cout << "Error al leer el archivo: " << e.what() << "\n";
+}
+catch(const std::filesystem::filesystem_error& e) {
+   std::cout << "Error al abrir el archivo " << e.path1() << ": " << e.what() << "\n";
+}
