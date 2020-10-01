22b27d20ee3d99e4529fcc531d3d8232504097d5
Luis Angel <luminothsparkle@gmail.com>
2020-10-01 07:02:55 -0600

    Agregada la captura de std::vector, la funcion analyze token y la creacion de los mensajes de error.

--- lemosius.cpp
+++ lemosius.cpp
@@ -14,6 +14,10 @@
 #include<utility>
 #include<array>
 #include<algorithm>
+#include<span>
+
+using path_source = std::pair<std::filesystem::path,std::span<char>>;
+using map_path_source = std::map<std::filesystem::path,std::span<char>>;
 
 struct compiler_error {     // cada vez que se eleva un error que es capturado en el driver, se construye el mensaje; en cuanto éste esté construido, nos podemos despreocupar de los recursos del programa
     static unsigned line_of(const token& t, const char* ini) {
@@ -25,28 +29,41 @@ struct compiler_error {     // cada vez que se eleva un error que es capturado e
     }
 
     static std::string_view view_line_of(const token& t, const char* first, const char* last) {
-        auto          data = std::find(std::reverse_iterator(t.source.begin( )),std::reverse_iterator(first),'\n').base( );
-        long unsigned len  = std::find(t.source.begin( ),                       last,                        '\n') - data;
-        return { data, len };
+        auto          data     = std::find(std::reverse_iterator(t.source.begin( )),std::reverse_iterator(first),'\n').base( );
+        long unsigned data_len = std::find(t.source.begin( ),                       last,                        '\n') - data;
+        return { data, data_len };
     }
 
-    static std::string_view view_of(const token& t, int len, const char* fin) {
-        return { t.source.begin( ), std::size_t(std::find(t.source.begin( ), (fin - t.source.begin( ) <= len ? fin : t.source.begin( ) + len), '\n') - t.source.begin( )) };
+    static std::string_view view_of(const token& t, long int len, const char* fin) {
+    	auto data = t.source.begin( );
+    	auto data_len = std::size_t(std::find(t.source.begin( ), std::min(fin - t.source.begin( ),len) + t.source.begin( ), '\n') - t.source.begin( ));
+        return { data , data_len };
     }
     
-    template<std::size_t N>  // si un error tiene dos puntos a reportar (ej. la redeclaración de un operador, el original y el incorrecto) podemos elevar un array<pair>
-    compiler_error(std::filesystem::path path, const std::vector<char>& source, const std::array<std::pair<token, std::string>, N>& ee) {
+    compiler_error(const map_path_source& compiled, const std::vector<std::pair<token, std::string>>& ee) {
+        auto obtain_file_data = [&compiled](const token& t) {
+            return std::find_if(compiled.begin(),compiled.end(),
+            [&t](const path_source& ps){
+                return &ps.second.front() <= t.source.data() && t.source.data() <= &ps.second.back();
+            });
+        };
         std::ostringstream oss;
         for(const auto& e : ee) {
-            oss << path.filename( ) << ":" << line_of(e.first, source.data( )) << ":" << col_of(e.first, source.data( )) << ": " << e.second << "\n";
-            //oss << "\t" << view_of(e.first, 10, source.data( ) + source.size( )) << "\n";
-            oss << "\t" << view_line_of(e.first, source.data( ), source.data( ) + source.size( )) << "\n";
+            token t           = e.first;
+            std::string mes   = e.second;
+            auto file_data    = *obtain_file_data(t);
+            auto file_name    = file_data.first.filename();
+            auto file_begin   = &file_data.second.front();
+            auto file_end     = &file_data.second.back();
+            oss << file_name << ":" << line_of(t, file_begin) << ":" << col_of(t, file_begin) << ": " << mes << "\n";
+            //oss << "\t" << view_of(t, 10, file_end) << "\n";
+            oss << "\t" << view_line_of(t, file_begin, file_end) << "\n";
         }
         what = std::move(oss).str( );
     }
 
-    compiler_error(const std::filesystem::path& path, const std::vector<char>& source, const std::pair<token, std::string>& e)
-    : compiler_error(path, source, std::array<std::pair<token, std::string>,1>({ e })) { }
+    compiler_error(const map_path_source& compiled, const std::pair<token, std::string>& e)
+    : compiler_error(compiled, std::vector<std::pair<token, std::string>>({ e })) { }
 
     compiler_error(const std::filesystem::path& path, const std::pair<std::exception,std::string>& e) {
         std::ostringstream oss;
@@ -57,15 +74,18 @@ struct compiler_error {     // cada vez que se eleva un error que es capturado e
     std::string what;
 }; // end struct compiler_error
 
-std::optional<program_resources> compile( std::filesystem::path path, std::set<std::filesystem::path>& compiled )
+std::optional<program_resources> compile( std::filesystem::path path, map_path_source& compiled )
 try {
     path = std::filesystem::absolute(path);
-    if( !compiled.insert( path ).second ) {
+    if( compiled.find( path ) != compiled.end( ) ) {
         return {};
     }
     program_resources pr;
     pr.source_path = path;
     pr.source_file = read_file( path );
+    
+    compiled.emplace(pr.source_path,pr.source_file);
+    
     try {
         const char* ini = pr.source_file.data( );
         // Lexico 1
@@ -77,28 +97,34 @@ try {
         // Compilacion recursiva
         auto cur_dir = std::filesystem::current_path( );
         std::filesystem::current_path(path.parent_path( ));
-	string_literal_parser str_par;        
-	for(const auto& inc : pr.tree.includes) {
+        for(const auto& inc : pr.tree.includes) {
             try {
-                if(auto res = compile(str_par.parse(inc.file_name), compiled); res.has_value( )) {
-                    pr.inclusions.push_back(program_resources::inclusion{is_public(inc), std::move(res.value( ))});
+                auto res = compile(unquote(inc.file_name), compiled);
+                if(res.has_value( )) {
+                    pr.inclusions.push_back(program_resources::inclusion{is_public(inc), std::move(res).value( )});
                 }
             }
-            catch(std::stack<compiler_error>& s) { // sí, la idea es que si ocurre un error en un include del include del include, empiece a juntar los mensajes de error en una pila
-                s.push(compiler_error(path, pr.source_file, std::make_pair(inc.file_name, "In file included")));
+            catch(std::stack<compiler_error>& s) {
+                s.push(compiler_error(compiled, std::make_pair(inc.file_name, "In file included")));
                 throw;
             }
         }
         std::filesystem::current_path(cur_dir);
+        // Semantico 1
+        analyze_operators(pr);
         // Lexico 2
         pr.program_tokens = lex.analisis( ini, END_OF_INPUT );
         tok_p = pr.program_tokens.data( );
         // Sintactico 2
         parse_program(tok_p,pr.tree);
+        
         return pr;
     }
-    catch(const std::pair<token, std::string>& e){ // un error en el archivo actual, lo envolvemos en una pila y lo reelevamos
-       throw std::stack<compiler_error>({ compiler_error( path, pr.source_file, e ) });
+    catch(const std::vector<std::pair<token, std::string>>& e){
+       throw std::stack<compiler_error>({ compiler_error( compiled, e ) });
+    }
+    catch(const std::pair<token, std::string>& e){
+       throw std::stack<compiler_error>({ compiler_error( compiled, e ) });
     }
 }
 catch(const std::filesystem::filesystem_error& e) {
@@ -114,7 +140,7 @@ try {
         std::cout << "Usage:" << argv[0] << " <path_file_name>";
         return 0;
     }
-    std::set<std::filesystem::path> compiled;
+    map_path_source compiled;
     std::cout << compile( argv[1], compiled ).value( );
 }
 catch(std::stack<compiler_error>& s) {
