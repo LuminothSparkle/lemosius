7d918ca4724be0f7177836bdeb59923b07bccd07
Luis Angel Buendia Arrieta <luminothsparkle@gmail.com>
2020-10-01 20:05:05 -0500

    Agregada la funcion to_string para apuntadores de tokens

--- parser_utilities.h
+++ parser_utilities.h
@@ -7,29 +7,29 @@
 #include<utility>
 #include<algorithm>
 
-bool is_access(token_type type) {
-   return type == PUBLIC_K || type == PRIVATE_K;
+bool is_access(const token& t) {
+   return t == PUBLIC_K || t == PRIVATE_K;
 }
 
-bool is_position(token_type type) {
-   return type == PREFIX_K || type == SUFFIX_K || type == INFIX_K;
+bool is_position(const token& t) {
+   return t == PREFIX_K || t == SUFFIX_K || t == INFIX_K;
 }
 
-bool is_asociativity(token_type type) {
-   return type == LEFT_K || type == RIGHT_K;
+bool is_asociativity(const token& t) {
+   return t == LEFT_K || t == RIGHT_K;
 }
 
-bool is_literal(token_type type) {
-   return type == IDENTIFIER_L || type == NUMBER_L;
+bool is_literal(const token& t) {
+   return t == IDENTIFIER_L || t == NUMBER_L;
 }
 
 //Aun por modificar y solo hecho para probar la gramatica.
-bool is_operator(token& t) {
+bool is_operator(const token& t) {
     return t == OPERATOR_L || t == ASSIGNMENT_O;
 }
 
 auto match_any(std::initializer_list<token_type> il) {
-   return [il](token_type t) {
+   return [&il](token_type t) {
       return std::find(il.begin(), il.end(), t) != il.end();
    };
 }
@@ -43,7 +43,7 @@ token* optional_match(token*& t, token_type type) {
     return optional_match(t, match_any({ type }));
 }
 
-// Template de match con predicado 
+// Template de match con predicado
 template<typename P>
 token* match(token*& t, P pred, const std::string& mes = "") {
     if(!pred(*t)) {
@@ -56,6 +56,8 @@ token* match(token*& t, token_type type, const std::string& mes = "") {
     return match(t, match_any({ type }), mes);
 } // end function match
 
-
+std::string to_string(token* t, const std::string& prefix = "" , const std::string& suffix = "") {
+    return (t != nullptr ? prefix + t->str( ) + suffix : "");
+}
 
 #endif
