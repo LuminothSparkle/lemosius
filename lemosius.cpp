11ba44078d64c5b6404bcdc4cdbf8316bc137d4f
Luis Angel Buendia Arrieta <luminothsparkle@gmail.com>
2020-10-01 20:10:35 -0500

    Quitado path_source, y utilizando los structured binding para mejorar la lectura del codigo

--- lemosius.cpp
+++ lemosius.cpp
@@ -7,7 +7,7 @@
 #include<cstddef>
 
 #include<iostream>
-#include<set>
+#include<map>
 #include<stack>
 #include<fstream>
 #include<filesystem>
@@ -16,7 +16,6 @@
 #include<algorithm>
 #include<span>
 
-using path_source = std::pair<std::filesystem::path,std::span<char>>;
 using map_path_source = std::map<std::filesystem::path,std::span<char>>;
 
 struct compiler_error {     // cada vez que se eleva un error que es capturado en el driver, se construye el mensaje; en cuanto éste esté construido, nos podemos despreocupar de los recursos del programa
@@ -29,33 +28,29 @@ struct compiler_error {     // cada vez que se eleva un error que es capturado e
     }
 
     static std::string_view view_line_of(const token& t, const char* first, const char* last) {
-        auto          data     = std::find(std::reverse_iterator(t.source.begin( )),std::reverse_iterator(first),'\n').base( );
-        long unsigned data_len = std::find(t.source.begin( ),                       last,                        '\n') - data;
+        auto        data     = std::find(std::reverse_iterator(t.source.begin( )),std::reverse_iterator(first),'\n').base( );
+        std::size_t data_len = std::find(t.source.begin( ),                       last,                        '\n') - data;
         return { data, data_len };
     }
 
-    static std::string_view view_of(const token& t, long int len, const char* fin) {
-    	auto data = t.source.begin( );
-    	auto data_len = std::size_t(std::find(t.source.begin( ), std::min(fin - t.source.begin( ),len) + t.source.begin( ), '\n') - t.source.begin( ));
+    static std::string_view view_of(const token& t, auto len, const char* fin) {
+    	auto        data     = t.source.begin( );
+    	std::size_t data_len = std::find(t.source.begin( ), std::min(fin - t.source.begin( ),len) + t.source.begin( ), '\n') - t.source.begin( );
         return { data , data_len };
     }
-    
+
     compiler_error(const map_path_source& compiled, const std::vector<std::pair<token, std::string>>& ee) {
         auto obtain_file_data = [&compiled](const token& t) {
-            return std::find_if(compiled.begin(),compiled.end(),
-            [&t](const path_source& ps){
+            return *std::find_if(compiled.begin(), compiled.end(),
+            [&t](const auto& ps){
                 return &ps.second.front() <= t.source.data() && t.source.data() <= &ps.second.back();
             });
         };
         std::ostringstream oss;
-        for(const auto& e : ee) {
-            token t           = e.first;
-            std::string mes   = e.second;
-            auto file_data    = *obtain_file_data(t);
-            auto file_name    = file_data.first.filename();
-            auto file_begin   = &file_data.second.front();
-            auto file_end     = &file_data.second.back();
-            oss << file_name << ":" << line_of(t, file_begin) << ":" << col_of(t, file_begin) << ": " << mes << "\n";
+        for(const auto& [t, mes] : ee) {
+            auto [file_path, file_data] = obtain_file_data(t);
+            auto file_begin = &file_data.front(), file_end = &file_data.back();
+            oss << file_path.filename() << ":" << line_of(t, file_begin) << ":" << col_of(t, file_begin) << ": " << mes << "\n";
             //oss << "\t" << view_of(t, 10, file_end) << "\n";
             oss << "\t" << view_line_of(t, file_begin, file_end) << "\n";
         }
@@ -81,25 +76,26 @@ try {
         return {};
     }
     program_resources pr;
-    pr.source_path = path;
-    pr.source_file = read_file( path );
-    
-    compiled.emplace(pr.source_path,pr.source_file);
-    
+    auto& [src_path, src_file, head_tokens, prog_tokens, tree, inclusions, operators] = pr;
+    src_path = path;
+    src_file = read_file( path );
+
+    compiled.emplace(src_path,src_file);
+
     try {
-        const char* ini = pr.source_file.data( );
+        const char* ini = src_file.data( );
         // Lexico 1
         lexer lex;
-        pr.header_tokens = lex.analisis( ini, PROC_K );
-        token* tok_p = pr.header_tokens.data( );
+        head_tokens = lex.analisis( ini, PROC_K );
+        token* tok_p = head_tokens.data( );
         // Sintactico 1
-        pr.tree = parse_header( tok_p );
+        tree = parse_header( tok_p );
         // Compilacion recursiva
         auto cur_dir = std::filesystem::current_path( );
         std::filesystem::current_path(path.parent_path( ));
-        for(const auto& inc : pr.tree.includes) {
+        for(const auto& inc : tree.includes) {
             try {
-                auto res = compile(unquote(inc.file_name), compiled);
+                auto res = compile(unquoted_str(inc.file_name), compiled);
                 if(res.has_value( )) {
                     pr.inclusions.push_back(program_resources::inclusion{is_public(inc), std::move(res).value( )});
                 }
@@ -113,11 +109,11 @@ try {
         // Semantico 1
         analyze_operators(pr);
         // Lexico 2
-        pr.program_tokens = lex.analisis( ini, END_OF_INPUT );
-        tok_p = pr.program_tokens.data( );
+        prog_tokens = lex.analisis( ini, END_OF_INPUT );
+        tok_p = prog_tokens.data( );
         // Sintactico 2
-        parse_program(tok_p,pr.tree);
-        
+        parse_program(tok_p,tree);
+
         return pr;
     }
     catch(const std::vector<std::pair<token, std::string>>& e){
@@ -137,7 +133,7 @@ catch(const std::ifstream::failure& e) {
 int main(int argc, char *argv[])
 try {
     if(argc != 2) {
-        std::cout << "Usage:" << argv[0] << " <path_file_name>";
+        std::cout << "Usage:" << argv[0] << " <path_file_name>\n";
         return 0;
     }
     map_path_source compiled;
