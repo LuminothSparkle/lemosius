62dacbae41cf67afec877a739997520dfe6d6bca
Luis Angel Buendia Arrieta <luminothsparkle@gmail.com>
2020-10-27 16:03:13 -0600

    Cambios menores, se movio la funcion to_string a string_utilities.h

--- parser_utilities.h
+++ parser_utilities.h
@@ -15,7 +15,7 @@ bool is_position(const token& t) {
    return t == PREFIX_K || t == SUFFIX_K || t == INFIX_K;
 }
 
-bool is_asociativity(const token& t) {
+bool is_associativity(const token& t) {
    return t == LEFT_K || t == RIGHT_K;
 }
 
@@ -49,10 +49,6 @@ token* match(token*& t, P pred, const std::string& mes = "") {
 
 token* match(token*& t, token_type type, const std::string& mes = "") {
     return match( t, match_any({ type }), mes );
-}
-
-std::string to_string(token* t, const std::string& prefix = "" , const std::string& suffix = "") {
-    return (t != nullptr ? prefix + t->str( ) + suffix : "");
 }
 
 #endif
