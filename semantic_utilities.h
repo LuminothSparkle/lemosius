d927ba880f215f85727732d7cef86906ae3d488d
Luis Angel Buendia Arrieta <luminothsparkle@gmail.com>
2020-10-27 16:05:19 -0600

    Se cambio la implementación de unquoted_str para verificar más errores de formación de strings, además se agrego get_representation para obtener los valores numericos de las literales

--- semantic_utilities.h
+++ semantic_utilities.h
@@ -7,6 +7,7 @@
 #include<string_view>
 #include<utility>
 #include<charconv>
+#include<cstdlib>
 
 template<typename T>
 bool is_public(const T& t) {
@@ -17,46 +18,91 @@ bool isoctal(char c) {
     return isdigit(c) && c < '8';
 }
 
-std::string unquoted_str(const token& t) {
-    auto throw_error = [&t](const auto& it) {
-        using namespace std::string_literals;
-        throw std::make_pair( token{t.type,{it,t.end()}}, "Semantic Error: String literal is ill-formed"s );
-    };
-    auto it = t.begin();
-    auto next = std::find(it,t.end( ),'\\');
-    std::string res(it,next);
-    it = next;
-    while(it != t.end( )) {
-        it = std::next(next);
-        constexpr std::string_view escaped = R"(abtnvfre0"'\)";
-        constexpr std::string_view replacement = "\a\b\t\n\v\f\r\x1B\0\"\'\\";
-        if(std::size_t idx = escaped.find(*it); idx < escaped.size( )) {
-            res.push_back(replacement[idx]);
+template<typename T>
+std::pair<T, bool> get_representation(const std::string_view& s) {
+    /*      :( GCC aún no tiene from_chars para floating point
+    long double res;
+    auto [end, error] = std::from_chars(s.data( ), s.data( ) + s.size( ), res);
+    return { T(res), (!error && p == s.end( ) && T(res) == res) }; */
+    std::string buffer(s);
+    auto p = const_cast<char*>(buffer.c_str());
+    long double res = std::strtold(p,&p);
+    if(p != buffer.c_str() + buffer.size()) {
+        return { T( ), false };
+    }
+    return { T(res), T(res) == res };
+    /*
+    try {      // horrible, pero más fácil de escribir que el que usa std::strold, en particular porque strold requiere una cadena con terminador
+       long double res = std::stold(std::string(s));      // nosotros sabemos que nuestros string_views vienen de tokens válidos y que acaban en algo que ya no es número,
+       return { T(res), T(res) == res };                  // pero prefiero no andarle jugando al vivo
+    } catch (...) {
+       return { T( ), false };
+    }
+    */
+}
+
+template<typename T>
+T get_representation(const token& t, const std::string& error_mes = "") {
+    auto [val, success] = get_representation<T>(t.source);
+    if(!success) {
+        throw std::make_pair( t, "Semantic Error:" + error_mes );
+    }
+    return val;
+}
+
+std::pair<std::string, std::string_view::iterator> unquoted_str(const std::string_view& sv) {
+    auto it = sv.begin();
+    if(sv.empty() || sv.front() != '"') { //Mejor para no tener problemas, ya que igual se checa en el lexer pero tambien aqui por si las dudas.
+        return { "", sv.begin() };
+    }
+    std::string res;
+    for(std::advance(it,1); it != sv.end() && *it != '"'; std::advance(it,1)) {
+        if(*it == '\\') {
             std::advance(it,1);
-        }
-        else if(bool is_hex = *it == 'x'; is_hex || isoctal(*it) ) {
-            std::size_t base = (is_hex ? 16 : 8);
-            std::advance(it,is_hex);
-            auto push_value = [&base,&t](auto& res, auto& it) {
-                unsigned char val = 0;
-                auto next = std::from_chars(it,t.end(),val,base).ptr;
-                res.push_back(val);
-                std::swap(next,it);
-                return next != it;
-            };
-            if(!push_value(res,it)) {
-                throw_error(it);
+            constexpr std::string_view escaped = R"(abtnvfre0"'\)";
+            constexpr std::string_view replacement = "\a\b\t\n\v\f\r\x1B\0\"\'\\";
+            if(std::size_t idx = escaped.find(*it); idx < escaped.size( )) {
+                res.push_back(replacement[idx]);
+                std::advance(it,1);
             }
-            while(is_hex && push_value(res,it));
+            else if(bool is_hex = *it == 'x'; is_hex || isoctal(*it) ) {
+                std::size_t base = (is_hex ? 16 : 8);
+                std::advance(it,is_hex);
+                auto obtain_char = [&base,&sv](auto& it,unsigned char& c) {
+                    unsigned char val = 0;
+                    auto first = it;
+                    it = std::from_chars(first,sv.end(),val,base).ptr;
+                    return first != it;
+                };
+                unsigned char c;
+                if(!obtain_char(it,c)) {
+                    return { res, it };
+                }
+                res.push_back(c);
+                while(is_hex && obtain_char(it,c)) {
+                    res.push_back(c);
+                }
+            }
+            else {
+                return { res, it };
+            }
         }
         else {
-            throw_error(it);
+            res.push_back(*it);
         }
-        next = std::find(it,t.end( ),'\\');
-        res.append(it,next);
-        it = next;
     }
-    return res.substr(1,res.size( ) - 2);
+    return { res, std::next(it,*it == '"') };
 }
+
+std::string unquoted_str(const token& t, const std::string& error_mes = "") {
+    using namespace std::string_literals;
+    auto [str, it] = unquoted_str(t.source);
+    if(it != t.end()) {
+        throw std::make_pair( token{ t.type, { it,t.end() } }, "Semantic Error:" + error_mes );
+    }
+    return str;
+}
+
+
 
 #endif
