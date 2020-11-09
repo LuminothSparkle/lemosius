f3b16f7f089df213fb765a4e040bb9865507e44b
Luis Angel Buendia Arrieta <luminothsparkle@gmail.com>
2020-11-09 16:24:29 -0600

    Se cambio la definicion para call_expression y para algunos mensajes de error

--- semantic_local.h
+++ semantic_local.h
@@ -2,57 +2,45 @@
 #define SEMANTIC_LOCAL_H
 
 #include "semantic_types.h"
-
 #include "semantic_utilities.h"
 
 #include <string>
 
-void analyze_expression( const expression& e, scope_stack& ss );
+void analyze_expression( const expression&, scope_stack& );
 
 void analyze_expression( const terminal_expression& e, scope_stack& ss ) {
-   if( *e.t == IDENTIFIER_L ) {
-      auto var = ss.find_symbol( *e.t ).get_variable();
-      if( var == nullptr ) {
-         throw std::pair<token, std::string>( *e.t, "Variable not declared" );
-      }
-      //...
+   if( *e.tok_ptr == IDENTIFIER_L && ss.find_symbol( *e.tok_ptr ).get_variable( ) == nullptr ) {
+      throw std::pair<token, std::string>( *e.tok_ptr, "Variable not declared." );
    }
 }
 
 void analyze_expression( const prefix_expression& e, scope_stack& ss ) {
-   analyze_expression( *e.ex, ss );
+   analyze_expression( *e.exp, ss );
 }
 
 void analyze_expression( const suffix_expression& e, scope_stack& ss ) {
-   analyze_expression( *e.ex, ss );
+   analyze_expression( *e.exp, ss );
 }
 
 void analyze_expression( const binary_expression& e, scope_stack& ss ) {
-   analyze_expression( *e.ex1, ss );
-   analyze_expression( *e.ex2, ss );
+   analyze_expression( *e.exp1, ss );
+   analyze_expression( *e.exp2, ss );
 }
 
 void analyze_expression( const call_expression& e, scope_stack& ss ) {
    auto sym = ss.find_symbol( *e.function_name );
-   const program_resources::visible_function* call = nullptr;
-   if( auto func_set = sym.get_function_overload_set(); func_set != nullptr ) {
-      if( auto it = func_set->find( e.params.size() ); it != func_set->end() ) {
-         call = &it->second;
-      }
+   if( auto var = sym.get_variable( ); var != nullptr ) {
+      throw std::vector<std::pair<token, std::string>>( {
+         { *e.function_name, "Variable used as function."  },
+         { *var,             "Variable declaration." }
+      } );
    }
-   if( call == nullptr ) {
-      if( auto var = sym.get_variable(); var != nullptr ) {
-         throw std::vector<std::pair<token, std::string>>( {
-            { *var,             "Variable declared here." },
-            { *e.function_name, "Used as function here."  }
-         } );
-      } else if( sym.get_function_overload_set() != nullptr ) {
-         throw std::pair<token, std::string>( *e.function_name, "Overload declared doesn't fit with number of arguments." );
-      } else {
-         throw std::pair<token, std::string>( *e.function_name, "Overload not declared." );
-      }
+   const auto& [user_ptr, builtin_ptr] = *sym.get_overload_sets( );
+   if( user_ptr == nullptr && builtin_ptr == nullptr ) {
+      throw std::pair<token, std::string>( *e.function_name, "Builtin function doesn't exist and an overload is not declared." );
+   } else if( ( user_ptr == nullptr || !user_ptr->contains( e.params.size( ) ) ) && ( builtin_ptr == nullptr || !builtin_ptr->contains( e.params.size( ) ) ) ) {
+      throw std::pair<token, std::string>( *e.function_name, "Builtin function doesn't exist and none of overloads declared fit the number of arguments." );
    }
-   //...
 }
 
 void analyze_expression( const expression& e, scope_stack& ss ) {       // sí, if else if !
@@ -69,14 +57,14 @@ void analyze_expression( const expression& e, scope_stack& ss ) {       // sí, 
    }
 }
 
-void analyze_statement( const statement& s, scope_stack& ss );
+void analyze_statement( const statement&, scope_stack& );
 
 void analyze_statement( const sequence_statement& s, scope_stack& ss ) {
-   ss.push();
+   ss.push( );
    for( const auto& stmt : s.body ) {
       analyze_statement( *stmt, ss );
    }
-   ss.pop();
+   ss.pop( );
 }
 
 void analyze_statement( const expression_statement& s, scope_stack& ss ) {
@@ -98,16 +86,15 @@ void analyze_statement( const if_statement& s, scope_stack& ss ) {
 }
 
 void analyze_statement( const var_statement& s, scope_stack& ss ) {
-   if( !ss.top().try_declare( *s.name ) ) {
+   if( !ss.top( ).try_declare( *s.name ) ) {
       throw std::vector<std::pair<token, std::string>>( {
-         { *ss.top().find_variable( *s.name ), "Variable redeclaration, first declaration." },
-         { *s.name,                            "Second declaration." }
+         { *s.name,                            "Variable redeclaration." },
+         { *ss.top( ).find_variable( *s.name ), "Previously declared." }
       } );
    }
    if( s.value != nullptr ) {
       analyze_expression( *s.value, ss );
    }
-
 }
 
 void analyze_statement( const return_statement& s, scope_stack& ss ) {
@@ -131,12 +118,12 @@ void analyze_statement( const statement& s, scope_stack& ss ) {      // sí, if 
 }
 
 void analyze_function( const function_declaration& f, scope_stack& ss ) {
-   ss.push();
+   ss.push( );
    for( const auto& var : f.parameters ) {
-      ss.top().try_declare( *var );
+      ss.top( ).try_declare( *var );
    }
    analyze_statement( *f.body, ss );
-   ss.pop();
+   ss.pop( );
 }
 
 #endif
